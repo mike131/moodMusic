43d6d81488c69fa308a365e0febd9e677e6b2d2e
Mike Lee <mikelee131@gmail.com>
2015-10-06 22:20:03 -0700

    Basic node server that will serve static assets from public directory

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/*jshint node:true*/
 var express = require('express'),
     app = express();
 
@@ -5,9 +6,12 @@ var express = require('express'),
 // Set Port
 app.set('PORT', (process.env.PORT || 5000));
 
+// Server public routes
+app.use(express.static('public'));
+
 var server = app.listen(app.get('PORT'), function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('MoodMusic listening at http://%s:%s', host, port);
 });

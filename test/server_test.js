7c15af8bf9c7e39761b795e528437d971b7b6921
Mike Lee <mikelee131@gmail.com>
2015-10-06 22:45:36 -0700

    Add a trivial test

--- test/server_test.js
+++ test/server_test.js
@@ -1,5 +1,8 @@
-var assert = require('assert');
+var assert = require('assert')
+    expect = require('expect.js');
 
 describe("Node Server", function() {
-  
+  it("should equal 1", function() {
+    expect(1).to.be(1);
+  });
 });

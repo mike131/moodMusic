7ba21ccf895f44f022a0a2151cbc17ec59c11a08
Mike Lee <mikelee131@gmail.com>
2015-10-10 16:45:45 -0700

    Change test to use chai

--- test/server_test.js
+++ test/server_test.js
@@ -1,8 +1,9 @@
-var assert = require('assert')
-    expect = require('expect.js');
+/*jshint mocha:true, node:true*/
+var chai = require('chai'),
+    expect = chai.expect;
 
 describe("Node Server", function() {
   it("should equal 1", function() {
-    expect(1).to.be(1);
+    expect(1).to.equal(1);
   });
 });

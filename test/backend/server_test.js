c6be94796c2c58eab440a8eb6b8342eaaec7c432
Mike Lee <mikelee131@gmail.com>
2015-10-14 22:00:05 -0700

    Moved files to different folders

---
+++ test/backend/server_test.js
@@ -0,0 +1,9 @@
+/*jshint mocha:true, node:true*/
+var chai = require('chai'),
+    expect = chai.expect;
+
+describe("Node Server", function() {
+  it("should equal 1", function() {
+    expect(1).to.equal(1);
+  });
+});

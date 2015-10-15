4af0e772460b6bb02ae864634e6807fd129b9c7c
Mike Lee <mikelee131@gmail.com>
2015-10-14 22:07:46 -0700

    Add a skeleton for frontend testing

---
+++ test/frontend/app_test.js
@@ -0,0 +1,12 @@
+/*jshint mocha:true*/
+
+var chai     =   require('chai'),
+    expect   =   chai.expect;
+
+describe('Frontend Application ', function () {
+  
+  describe('AngularJS', function () {
+    
+  });
+
+});
